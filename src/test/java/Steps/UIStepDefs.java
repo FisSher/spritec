825afef44decf61dbb1c5d88e8d6ce4ef8a6da30
Facundo <facundo.paolini@spritecloud.com>
2022-02-14 12:23:43 +0100

    added 2 os

--- src/test/java/Steps/UIStepDefs.java
+++ src/test/java/Steps/UIStepDefs.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,14 +16,34 @@ public class UIStepDefs {
 
     @Given("the browser is at the main page")
     public void theBrowserIsAtTheMainPage() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
+
+
+        boolean OS = System.getProperty("os.name").startsWith("Linux");
+        if (OS) {
+            // String currentDir = System.getProperty("user.dir");
+            // System.setProperty("webdriver.chrome.driver", currentDir + "/Driver/chromedriver");
+
+            ChromeOptions opt = new ChromeOptions();
+            opt.addArguments("disable-infobars"); // disabling infobars
+            opt.addArguments("--disable-extensions"); // disabling extensions
+            opt.addArguments("--no-sandbox"); // Bypass OS security model
+            opt.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            opt.addArguments("--window-size=1920,1080");
+            opt.addArguments("--disable-gpu");
+            opt.addArguments("--proxy-server='direct://'");
+            opt.addArguments("--proxy-bypass-list=*");
+            opt.addArguments("--start-maximized");
+            opt.addArguments("--headless");
+            driver = new ChromeDriver(opt);
+        } else {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+
         driver.get("https://demoqa.com/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
-
-
 
 
     @Given("the practice form is shown")
